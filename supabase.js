ba0ea6d5731913e1e5e1aaf3d7a6993cf94cd0ea
fazanv794 <fazanv794@gmail.com>
2025-12-19 13:35:06 +0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = 'https://ejbdehqypaotjnsiunny.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqYmRlaHF5cGFvdGpuc2l1bm55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MDg2MzQsImV4cCI6MjA4MDk4NDYzNH0.hDWtZLUtMgg266d4LpFsKaOfKF1zAPt1JFN8OtqbLFk'
 
 // Инициализация Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+
 
 // Простая функция уведомления
 function showNotification(message, type = 'info') {
